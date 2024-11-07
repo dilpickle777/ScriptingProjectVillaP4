bdeaae303125038bf29e00edd5513cfc18d22e6c
dilpickle777 <727696@student.dusd.net>
2024-11-07 10:30:14 -0800

    added script
    added awake and start script

---
+++ ScriptingProjectP4Villa/Assets/Scripts/AwakeandStart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeandStart : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }      
+    
+}
