978511593ee18f1ee4dda9f97bd76a0aa3b19fb3
dilpickle777 <727696@student.dusd.net>
2025-01-22 10:29:33 -0800

    lookat
    I made the camera follow my sphere while falling

---
+++ ScriptingProjectP4Villa/Assets/Scripts/CameraLookAt.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class CameraLookAt : MonoBehaviour
+{
+   
+    
+        public Transform target;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
