0d0fb3e67a0068732be66344a3a363a39fb38e8b
dilpickle777 <727696@student.dusd.net>
2025-04-02 11:40:19 -0700

    addes nwew script
    addedd a tasnk and shootinh

---
+++ ScriptingProjectP4Villa/Assets/Scripts/MovementControls.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class MovementControls : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+
+
+    void Update()
+    {
+        Movement();
+    }
+
+
+    void Movement()
+    {
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.up * turnMovement);
+    }
+}
